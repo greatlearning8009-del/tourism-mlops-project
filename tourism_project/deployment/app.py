98eedacd49c1c5c1761e582558fb92d9b7271efb
greatlearning8009-del <greatlearning.8009@gmail.com>
2026-06-18 10:52:22 +0530

    Update app.py

--- tourism_project/deployment/app.py
+++ tourism_project/deployment/app.py
@@ -9,7 +9,7 @@ from huggingface_hub import hf_hub_download
 # --------------------------------------------------
 
 model_path = hf_hub_download(
-    repo_id="jyotibudharapu/tourism-model",
+    repo_id="SRGL/tourism-model",
     filename="tourism_model_v1.joblib"
 )
 
